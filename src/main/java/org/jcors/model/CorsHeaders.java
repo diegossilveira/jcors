cf47a3efc6e0a10a4f80a4b198e2a972c434c484
diegossilveira <diegossilveira@gmail.com>
2011-05-24 18:03:02 -0300

    Adding more CORS Headers

--- src/main/java/org/jcors/model/CorsHeaders.java
+++ src/main/java/org/jcors/model/CorsHeaders.java
@@ -13,8 +13,11 @@ public final class CorsHeaders {
 	public static final String ORIGIN_HEADER = "Origin";
 	public static final String ACCESS_CONTROL_ALLOW_ORIGIN_HEADER = "Access-Control-Allow-Origin";
 	public static final String ACCESS_CONTROL_ALLOW_CREDENTIALS = "Access-Control-Allow-Credentials";
-	public static final String ACCESS_CONTROL_EXPOSE_HEADERS = "Access-Control-Expose-Headers";
+	public static final String ACCESS_CONTROL_ALLOW_METHODS = "Access-Control-Allow-Methods";
+	public static final String ACCESS_CONTROL_ALLOW_HEADERS = "Access-Control-Allow-Headers ";
 	public static final String ACCESS_CONTROL_REQUEST_METHOD = "Access-Control-Request-Method";
 	public static final String ACCESS_CONTROL_REQUEST_HEADERS = "Access-Control-Request-Headers";
+	public static final String ACCESS_CONTROL_EXPOSE_HEADERS = "Access-Control-Expose-Headers";
+	public static final String ACCESS_CONTROL_MAX_AGE = "Access-Control-Max-Age";
 
 }
