9ea3ec57b738e6e73a6f04e30a439743118c28d2
diegossilveira <diegossilveira@gmail.com>
2011-05-24 18:38:59 -0300

    Implementing the CORS-enabling Java filter

--- src/main/java/org/jcors/web/filter/CorsEnablingFilter.java
+++ src/main/java/org/jcors/web/filter/CorsEnablingFilter.java
@@ -8,9 +8,13 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
 import org.jcors.config.JCorsConfiguration;
+import org.jcors.web.RequestHandler;
+import org.jcors.web.RequestHandlerFactory;
 
 /**
  * Main application filter, responsible for enabling CORS Requests handling
@@ -24,18 +28,31 @@ public class CorsEnablingFilter implements Filter {
 	private JCorsConfiguration config;
 
 	public void init(FilterConfig config) throws ServletException {
-		// TODO Auto-generated method stub
-
+		//TODO: parse XML configuration file, if existing in classpath
+		this.config = new JCorsConfiguration();
 	}
 
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-		// TODO Auto-generated method stub
+		
+		HttpServletRequest httpRequest = (HttpServletRequest) request; 
+		HttpServletResponse httpResponse = (HttpServletResponse) response;
+		
+		try {
+			
+			RequestHandler requestHandler = RequestHandlerFactory.getRequestHandler(httpRequest);
+			requestHandler.handle(httpRequest, httpResponse, config);
+			
+		} catch(Exception ex) {
+			
+			log.error("Error while handling a request");
+			throw new ServletException(ex.getMessage(), ex);
+		}
+		
+		chain.doFilter(request, response);
 
 	}
 
 	public void destroy() {
-		// TODO Auto-generated method stub
-
 	}
 
 }
