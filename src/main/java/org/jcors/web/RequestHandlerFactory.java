4bae2049b456a2af0fbbcc250a9c92b161fe199a
diegossilveira <diegossilveira@gmail.com>
2011-07-05 23:21:43 -0300

    Improving debug log information details

--- src/main/java/org/jcors/web/RequestHandlerFactory.java
+++ src/main/java/org/jcors/web/RequestHandlerFactory.java
@@ -6,37 +6,47 @@ import org.apache.log4j.Logger;
 import org.jcors.model.CorsHeaders;
 
 /**
- * Class that produces the correct instance for CORS requests handling 
+ * Class that produces the correct instance for CORS requests handling
  * 
  * @author Diego Silveira
  */
 public final class RequestHandlerFactory {
 
 	private static final Logger log = Logger.getLogger(RequestHandlerFactory.class);
-	
+
+	private static final String PREFLIGHT_REQUEST_TYPE = "preflight";
+	private static final String ACTUAL_REQUEST_TYPE = "actual";
+	private static final String NON_CORS_REQUEST_TYPE = "non-CORS";
+
 	public static RequestHandler getRequestHandler(HttpServletRequest request) {
-	
+
 		// CORS Request
-		if(!isEmptyHeader(request, CorsHeaders.ORIGIN_HEADER)) {
-			
-			if(!isEmptyHeader(request, CorsHeaders.ACCESS_CONTROL_REQUEST_METHOD_HEADER)) {
-				log.debug("Handling preflight request");
+		if (!isEmptyHeader(request, CorsHeaders.ORIGIN_HEADER)) {
+
+			if (!isEmptyHeader(request, CorsHeaders.ACCESS_CONTROL_REQUEST_METHOD_HEADER)) {
+				logRequest(PREFLIGHT_REQUEST_TYPE, request);
 				return new PreflightRequestHandler();
 			}
-			
-			log.debug("Handling actual request");
+
+			logRequest(ACTUAL_REQUEST_TYPE, request);
 			return new ActualRequestHandler();
-			
+
 		}
-		
-		log.debug("Handling non-CORS request");
+
+		logRequest(NON_CORS_REQUEST_TYPE, request);
 		return new SimpleRequestHandler();
 	}
-	
+
 	private static boolean isEmptyHeader(HttpServletRequest request, String header) {
-		
+
 		String headerValue = request.getHeader(header);
 		return headerValue == null || "".equals(headerValue.trim());
 	}
-	
+
+	private static void logRequest(String requestType, HttpServletRequest request) {
+		
+		String message = String.format("Handling %s request: %s (%s)", requestType, request.getRequestURI(), request.getMethod());
+		log.debug(message);
+	}
+
 }
